89ebcbdc5613cc28ba6ff3f6c2f2d115683facba
cis290 <cis290@PC22750-2142.local>
2016-12-01 15:30:57 -0600

    cs

---
+++ Dream Catcher/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Dream Catcher
+//
+//  Created by cis290 on 12/1/16.
+//  Copyright © 2016 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var dreamcatcher: UIImageView!
+    
+    @IBOutlet weak var details: UITextView!
+    
+    @IBAction func createaccount(sender: UIButton) {
+    }
+    
+    @IBAction func login(sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
